d40965783e65fa76b42b5f95a1fecfd494d41fec
Cory <coryaccounts@outlook.com>
2017-01-25 15:34:56 -0600

    Initial 1.0 commit
    It seems to work correctly.

---
+++ config.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace BotMaker
+{
+    partial class config
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(config));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.txtGame = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Audio Files Location:";
+            // 
+            // txtDir
+            // 
+            this.txtDir.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDir.Location = new System.Drawing.Point(13, 26);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(259, 20);
+            this.txtDir.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Prefix Character:";
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPrefix.Location = new System.Drawing.Point(12, 65);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(259, 20);
+            this.txtPrefix.TabIndex = 3;
+            this.txtPrefix.TextChanged += new System.EventHandler(this.txtPrefix_TextChanged);
+            // 
+            // txtGame
+            // 
+            this.txtGame.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtGame.Location = new System.Drawing.Point(12, 104);
+            this.txtGame.Name = "txtGame";
+            this.txtGame.Size = new System.Drawing.Size(259, 20);
+            this.txtGame.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "\"Playing\":";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtToken.Location = new System.Drawing.Point(12, 143);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(259, 20);
+            this.txtToken.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Bot Token:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(197, 169);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // config
+            // 
+            this.AcceptButton = this.btnSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 203);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtGame);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDir);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "config";
+            this.Text = "Config";
+            this.Load += new System.EventHandler(this.config_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.TextBox txtGame;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
---
+++ config.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace BotMaker
+{
+    public partial class config : Form
+    {
+        public config()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPrefix_TextChanged(object sender, EventArgs e)
+        {
+            if(txtPrefix.Text.Length > 1)
+            {
+                txtPrefix.Text = txtPrefix.Text.ToCharArray()[txtPrefix.Text.Length-1] + "";
+                txtPrefix.SelectAll();
+            }
+        }
+
+        private void config_Load(object sender, EventArgs e)
+        {
+            txtDir.Text = ReadSetting("audiodir");
+            txtPrefix.Text = ReadSetting("prefixchar");
+            txtGame.Text = ReadSetting("game");
+            txtToken.Text = ReadSetting("bottoken");
+        }
+
+        public static String ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "ERR";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return "ERR";
+            }
+        }
+
+        public static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            AddUpdateAppSettings("audiodir", txtDir.Text);
+            AddUpdateAppSettings("prefixchar", txtPrefix.Text);
+            AddUpdateAppSettings("game", txtGame.Text);
+            AddUpdateAppSettings("bottoken", txtToken.Text);
+            this.Close();
+        }
+    }
+}
